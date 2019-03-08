85a02d4b04f8a2987002b61f4bd91f3a9a1b72c8
vijaya <vlpopuri@hotmail.com>
2019-03-08 14:25:40 -0800

    sum

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var added = a + b;
+  var return_string = 'The sum of ' + a + ' and ' + b + ' is ' + added + '.';
+  console.log( 'added: ' + added + '.');
+ return [ added, return_string];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
